94b4860b606019cfdb1daccc4bbb4669382137db
EuniceCeja <eceja93@hotmail.com>
2023-12-08 18:48:36 -0600

    Javascript

---
+++ javascript/script.js
@@ -0,0 +1,57 @@
+// comentarios de una sola línea
+
+/* comentarios 
+multilínea */
+
+//alert ("Hola mundo");
+
+//console.log("Este es un mensaje para mi consola")
+
+//mensajes en pantalla
+/* var nombre = prompt("Ingresa tu nombre");
+alert("Hola, "+ nombre + " Bienvenido a la ch35"); */
+
+/* var numero1 = parseInt (prompt("Ingresa el primer numero:"));
+var numero2 = parseInt (prompt("Ingresa el segundo numero:"));
+
+console.log("suma:" + (numero1 + numero2));
+console.log("resta:" + (numero1 - numero2));
+console.log("multiplicación:" + (numero1 * numero2));
+console.log("división:" + (numero1 / numero2));
+*/
+
+/* var edad = parseInt (prompt("Ingresa tu edad:"));
+ 
+if (edad < 18) {
+    alert("Eres menor de edad.");
+}
+else if (edad >= 18) {
+    alert("Eres mayor de edad!");
+}
+else {
+    alert("Ingresa un numero válido");
+}
+*/
+
+// TIPOS DE DATOS Y VARIABLES
+
+// scope es el alcance de nuestras variables
+
+/*var, let, const la diferencia es el alcance
+var tiene alcance global
+let tiene alcance local
+const se mantiene sin ningún cambio */
+
+/* string : cadena de texto (lleva comillas)
+number : numero 
+boolean : true/false
+null : nulo
+undefinied : no está asignado o definido */
+
+let nombre = "Daniel";
+let invitadxsExtras = 2;
+let esMayorEdad = true;
+let edad = 25; 
+let invitadxEspecial = null;
+let horaDeSalida = undefined;
+
